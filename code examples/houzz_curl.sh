ad6cb819d1fed9cc49d4f1fda7c47012d8354c84
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:28:28 +0200

    Adding code examples/houzz_curl.sh

---
+++ code examples/houzz_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.houzz.com/photos/kitchen-ideas-and-designs-phbr0-bp~t_709"}'
